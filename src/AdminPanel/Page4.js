7a2f90d691ead1a8d36e0bcdaf33e61af58b051b
Otabek <133384666+ABDURAHIMhalilov@users.noreply.github.com>
2023-10-09 12:57:33 +0300

    ss
    s

--- src/AdminPanel/Page4.js
+++ src/AdminPanel/Page4.js
@@ -1,8 +1,35 @@
-import React from "react";
-import {AiOutlineArrowLeft} from 'react-icons/ai'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function Page4() {
-  function handlePress() {
+  const [state, setState] = useState([]);
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userAddress, setUserAddress] = useState("");
+  const [userID, setUserID] = useState(null);
+  const [chkbox, setChkbox] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/auth/users", {
+        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+      })
+      .then((res) => {
+        setState(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  function handlePress(key) {
+    setUserID(key.id);
+    setUserEmail(key.email);
+    setUserAddress(key.address);
+    setChkbox(key.manager);
+    setUserPassword(key.password);
+    document.querySelector("#emailInput").value = key.email;
+    document.querySelector("#addressInput").value = key.address;
     document.querySelector(".MainS").style = "display: none";
     document.querySelector(".opacityDiv").style = "display: block";
   }
@@ -11,65 +38,120 @@ export default function Page4() {
     document.querySelector(".MainS").style = "display: block";
     document.querySelector(".opacityDiv").style = "display: none";
   }
+
+  function putUser() {
+    var address = document.querySelector(".addressInput").value;
+    var email = document.querySelector(".emailInput").value;
+    console.log(address, email, chkbox, localStorage.getItem("password"));
+    var data = new FormData();
+    data.append("email", email);
+    data.append("address", address);
+    data.append("manager", chkbox);
+    data.append("password", userPassword);
+    axios
+      .put(`http://localhost:5000/auth/users/${userID}`, data, {
+        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+      })
+      .then((res) => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <div>
       <div className="opacityDiv">
-      <h4 onClick={() => handlePress2()}><AiOutlineArrowLeft style={{ fontSize: 30, cursor: 'pointer' }} /></h4>
-      <div style={{ width: '50%', margin: 'auto' }}>
-        <h2>Пользователь</h2>
-        <div>
-          <div style={{  }}>
-            <p style={{ height: 7 }}>email</p>
-            <input type="text" style={{ width: '100%', height: 40, borderRadius: 4, border: '1px solid gray' }} />
-          </div>
+        <h4 onClick={() => handlePress2()}>
+          <AiOutlineArrowLeft style={{ fontSize: 30, cursor: "pointer" }} />
+        </h4>
+        <div style={{ width: "50%", margin: "auto" }}>
+          <h2>Пользователь</h2>
+          <div>
+            <div style={{}}>
+              <p style={{ height: 7 }}>email {userEmail}</p>
+              <input
+                type="text"
+                style={{
+                  width: "100%",
+                  height: 40,
+                  borderRadius: 4,
+                  border: "1px solid gray",
+                }}
+                id="emailInput"
+                className="emailInput"
+              />
+            </div>
 
-          <div style={{  }}>
-            <p style={{ height: 7 }}>Адрес</p>
-            <input type="text" style={{ width: '100%', height: 40, borderRadius: 4, border: '1px solid gray' }} />
-            <div style={{ marginTop: 10, marginBottom: 10 }}>
-              <input type="checkbox" />
-              Сделать менеджером
+            <div style={{}}>
+              <p style={{ height: 7 }}>
+                Адрес {userAddress === null ? "null" : userAddress}
+              </p>
+              <input
+                type="text"
+                style={{
+                  width: "100%",
+                  height: 40,
+                  borderRadius: 4,
+                  border: "1px solid gray",
+                }}
+                id="addressInput"
+                className="addressInput"
+              />
+              <div style={{ marginTop: 10, marginBottom: 10 }}>
+                <input
+                  type="checkbox"
+                  defaultChecked={chkbox}
+                  onChange={() => setChkbox(true)}
+                />
+                Сделать менеджером
+              </div>
             </div>
           </div>
+          <button
+            style={{
+              background: "#800000",
+              height: 40,
+              width: 150,
+              border: "none",
+              borderRadius: 5,
+              color: "white",
+              fontSize: 17,
+            }}
+            onClick={() => putUser()}
+          >
+            Обновить
+          </button>
         </div>
-        <button style={{ background: '#800000', height: 40, width: 150, border: 'none', borderRadius: 5, color: 'white', fontSize: 17 }}>Обновить</button>
       </div>
-      </div>
-      <div className="MainS" style={{ width: "100%" }}>
+      <div
+        className="MainS"
+        style={{ width: "100%", height: "100vh", overflow: "hidden" }}
+      >
         <div className="adminPage1">
           <h1>Все пользователи</h1>
         </div>
         <div className="bigOrder">
           <div className="mainOrder">
-            <div className="minOrder" onClick={() => handlePress()}>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  width: "30%",
-                }}
-              >
-                <img src="https://github.com/twbs.png" alt="" />
-                <h1 style={{ width: "90%" }}>webabbas9@gmail.com</h1>
-              </div>
-              <p>Менеджер</p>
-              <p style={{ marginRight: 7 }}>Редактировать</p>
-            </div>
-            <div className="minOrder" onClick={() => handlePress()}>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  width: "30%",
-                }}
-              >
-                <img src="https://github.com/twbs.png" alt="" />
-                <h1 style={{ width: "90%" }}>webabbas9@gmail.com</h1>
-              </div>
-              <p style={{ marginRight: 7 }}>Редактировать</p>
-            </div>
+            {state.map((item) => {
+              return (
+                <div className="minOrder" onClick={() => handlePress(item)}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      width: "30%",
+                    }}
+                  >
+                    <img src="https://github.com/twbs.png" alt="" />
+                    <h1 style={{ width: "90%" }}>{item.email}</h1>
+                  </div>
+                  <p>{item.manager === true ? "Менеджер" : ""}</p>
+                  <p style={{ marginRight: 7 }}>Редактировать</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
